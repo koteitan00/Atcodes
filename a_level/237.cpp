dfab01d42bdf55d5b3c3385c365327d5a73658bf
koteitan00 <hoshikairi00@gmail.com>
2022-11-25 23:32:48 +0900

    added 237.cpp

---
+++ a_level/237.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <string>
+#include<algorithm>
+using namespace std;  
+
+int main(){
+  long long int n;cin >> n;
+  if(n >= pow(2,31) || n < pow(2,31)*-1)cout << "No";
+  else cout << "Yes";
+  }
