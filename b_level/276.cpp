7462375329fe8ae9862a2144e0b036b9d168b658
koteitan00 <hoshikairi00@gmail.com>
2022-12-04 23:59:20 +0900

    nothing

---
+++ b_level/276.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
+using namespace std;
+#define rep(i,m) for(int i=0;i<m;i++)
+
+int main(){
+  int n,m;cin >> n >> m;
+  rep(i,m){
+  
+  }
+ }
